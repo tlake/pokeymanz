a7d592415223f3a348fbe9816a437e0dd7b91ebc
tlake <tanner.lake@gmail.com>
2015-01-09 23:47:29 -0800

    .gitignore, types.py; minor update to readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pokeymanz
 A project for playing with Python3 and practicing classes and inheritance
+
+### Note: This project uses Python 3.4.0
+
---
+++ types.py
@@ -0,0 +1,22 @@
+def Type(object):
+    pass
+
+
+
+def Fire(Type):
+
+    def __init__(self):
+        self.type = "Fire"
+
+
+
+def Grass(Type):
+
+    def __init__(self):
+        self.type = "Grass"
+
+
+
+def Water(Type):
+
+    def __init__(self):
